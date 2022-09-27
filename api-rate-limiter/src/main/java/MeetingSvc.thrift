06ed3313b625dec672e2b6adb53bcb5b21e29ede
12shivamkumar <shivam.kumar@capillarytech.com>
2022-09-27 12:28:51 +0530

    changes were on api-rate-limiter folder

--- api-rate-limiter/src/main/java/MeetingSvc.thrift
+++ api-rate-limiter/src/main/java/MeetingSvc.thrift
@@ -3,4 +3,6 @@ service MeetingSvc
     bool cancelMeetingOfRemovedEmployee(1:string employeeId);
 
     bool updateStatusOfRemovedEmployee(1:string employeeId);
+
+    bool isAlive();
 }
---
+++ api-rate-limiter/src/main/java/develop/base.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `audience_groups` (
+  `id` int(11) NOT NULL,
+  `campaign_id` int(11) NOT NULL,
+  `org_id` int(11) NOT NULL DEFAULT '0',
+  `audience_provider` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `params` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` enum('LOYALTY','NON_LOYALTY','ALL') CHARACTER SET utf8mb4 NOT NULL DEFAULT 'LOYALTY',
+  PRIMARY KEY (`id`,`org_id`),
+  KEY `campaign_id` (`campaign_id`,`audience_provider`),
+  KEY `type` (`org_id`,`type`)
+) ;
+
